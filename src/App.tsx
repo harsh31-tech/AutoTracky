07667c663c99a6668844c79f573cd11c33be13e6
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-09-11 17:10:19 +0530

    register route  is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Landing from "./pages/Landing";
-import Login from "./pages/Login";
 import Register from "./pages/Register";
-import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <header className="app-header">
-        <Link to="/">autoTracky</Link>
-        <nav aria-label="Main navigation">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/dashboard">Dashboard</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
